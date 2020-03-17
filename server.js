339dfa910aaf64a25cf6fbe294f045e6ec68cae3
Gabriele Mittica <mittica.gabriele@gmail.com>
2020-03-17 12:41:28 +0100

    pipeline test 2

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require('express'),
     port = process.env.PORT || 3000;
 
 app.get('/', function(req, res){
-  res.send('Webserver versione 3 con PIPELINE')
+  res.send('Webserver versione 4 con PIPELINE')
 })
 
 app.listen(port, function(){
